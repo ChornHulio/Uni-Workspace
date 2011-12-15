7da40bf53e24a928483db1eeb9afb17e05f7b233
ChornHulio <chornhulio@web.de>
2011-12-15 15:47:49 +0100

    MMU works, but have a problem with fragmentation

--- Systemnahe Software I/Aufgabe10/my_alloc.c
+++ Systemnahe Software I/Aufgabe10/my_alloc.c
@@ -10,7 +10,7 @@
 #include "my_alloc.h"
 #include "my_system.h"
 
-#define WASTE_RATE 0.03
+#define WASTE_RATE 0.97
 
 unsigned int mem_size = 0;
 unsigned int used_mem = 0;
@@ -28,7 +28,6 @@ void init_my_alloc ()
 	mem_size = BLOCKSIZE;
 	// mark the memory as free
 	*(data_start) = 0; // unused
-	//*(data_start+1) = (short) (BLOCKSIZE - 16); // the next 8176 bytes are unused
 	*(data_start+1) = (BLOCKSIZE - 16) / 0xFF; // ...
 	*(data_start+2) = (BLOCKSIZE - 16) % 0xFF; // the next 8176 bytes are unused
 	data_current = data_start;
@@ -39,11 +38,11 @@ void * my_alloc (int size)
 {
 	do
 	{
-		while (offset >= 0 && offset < (BLOCKSIZE - 8)) 
+		while (offset >= 0 && offset < (BLOCKSIZE - size - 8)) 
 		{
 			char status = (char) * (data_current + offset);
 			unsigned short bytes = (unsigned char)*(data_current + offset + 1) * 0xFF + (unsigned char)*(data_current + offset + 2);
-			if(status == 0 && bytes > size)  // block is unused and big enough
+			if(status == 0 && bytes >= size)  // block is unused and big enough
 			{
 				found_something = 1;
 				used_mem += size;
@@ -56,7 +55,7 @@ void * my_alloc (int size)
 					*(data_current + offset + 9 + size) = (bytes - size) / 0xFF; // ...
 					*(data_current + offset +10 + size) = (bytes - size) % 0xFF; // the next 'x' bytes are unsed
 				}
-				void * ret = data_current + offset + 8;
+				void * ret = data_current + (offset + 8)*sizeof(char);
 				offset += 8 + size;
 				return ret;
 			}
@@ -70,11 +69,11 @@ void * my_alloc (int size)
 		{
 			data_current = data_start; // go back to the beginning
 			found_something = 0;
-			current_block = 0;
+			current_block = 1;
 		}
 		else
 		{
-			data_current = (void*) data_current + (BLOCKSIZE - 8);
+			data_current = (void*) data_current + sizeof(BLOCKSIZE - 8);
 			current_block++;
 		}
 	} while(found_something == 1 && (used_mem / mem_size) < WASTE_RATE);
@@ -85,7 +84,8 @@ void * my_alloc (int size)
 	blocks++;
 	current_block++;
 	data_current = get_block_from_system();
-	*(data_last + (BLOCKSIZE - 8)) = data_current; //BUG
+	char * end = data_last + BLOCKSIZE - 8;
+	end = data_current;
 	mem_size += BLOCKSIZE;
 	// mark the memory as free
 	*(data_current) = 0; // unused
@@ -99,15 +99,5 @@ void my_free (void * ptr)
 	char * cptr = ptr - 8 * sizeof(char);
 	unsigned short bytes = (unsigned char)*(cptr + 1) * 0xFF + (unsigned char)*(cptr + 2);
 	*(cptr) = 0; // unused
-	
-	/* BUG: next_bytes can be the end of a block
-	char status = (char)*(cptr + 8 + bytes);
-	short next_bytes = (unsigned char)*(cptr + bytes + 9) * 0xFF + (unsigned char)*(cptr + bytes + 10);
-	
-	if(status == 0 && next_bytes > 0)
-	{
-		*(cptr + 1) = (bytes + next_bytes) / 0xFF; // ...
-		*(cptr + 2) = (bytes + next_bytes) % 0xFF; // next x bytes are unused
-	}
-	*/
+	// BUG: fragmenting
 }
