410d9dc2c8c22ae96c1abc9eb8688f5fa6fc06c2
ChornHulio <chornhulio@web.de>
2012-02-06 21:39:38 +0100

    deleted half-done stuff

--- Funktionale Programmierung/haskellExRecursion.hs
+++ Funktionale Programmierung/haskellExRecursion.hs
@@ -14,3 +14,47 @@ and' (False:xs) = False
 concat' :: [[a]] -> [a]
 concat' = foldr (++) []
 
+-- produce a list with n identical elements
+replicate' :: Int -> a -> [a]
+replicate' 0 _ = []
+replicate' n x = x:(replicate' (n-1) x)
+
+-- select the n'th element of a list
+select :: [a] -> Int -> a
+select (x:xs) 1 = x 
+select (x:xs) n = (!!) xs (n-1) 
+
+-- decide if a value is an element of a list
+elem' :: Eq a => a -> [a] -> Bool
+elem' _ [] = False
+elem' x (y:ys) | x == y = True
+              | otherwise = elem x ys
+
+-- merge and order two sorted lists
+mergeOrd :: Ord a => [a] -> [a] -> [a]
+mergeOrd [] ys = ys
+mergeOrd xs [] = xs
+mergeOrd (x:xs) (y:ys) | x < y = x:(mergeOrd xs (y:ys))
+                       | otherwise = y:(mergeOrd (x:xs) ys)
+                       
+-- merge sort
+mergeSort :: Ord a => [a] -> [a]
+mergeSort [x] = [x]
+mergeSort xs = mergeOrd (mergeSort (fst (halve xs))) (mergeSort (snd (halve xs)))
+halve :: [a] -> ([a],[a])
+halve xs = (take (length xs `div` 2) xs, drop (length xs `div` 2) xs)
+
+-- sum with recursion
+sum' :: Integral a => [a] -> a
+sum' xs = foldr (+) 0 xs
+
+-- take with recursion
+take' :: Int -> [a] -> [a]
+take' 0 _ = []
+take' _ [] = []
+take' n (x:xs) = x:(take' (n-1) xs)
+
+-- last element of a list
+last' :: [a] -> a
+last' [x] = x
+last' (x:xs) = last xs
