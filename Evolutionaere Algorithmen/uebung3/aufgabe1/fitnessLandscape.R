1b568216fc7f42df70bc212bebfd0805bf36d7b7
ChornHulio <chornhulio@web.de>
2012-05-29 19:23:50 +0200

    2D-Landscape and perceptron via EA-algorithm

---
+++ Evolutionaere Algorithmen/uebung3/aufgabe1/fitnessLandscape.R
@@ -0,0 +1,92 @@
+initPopulation <- function(candidates,gene,min,max) {
+	population <- matrix(nrow=candidates, ncol=(gene+1))
+	for(i in 1:candidates) {
+		population[i] <- runif(gene,min,max)
+		population[i,gene+1] <- 0
+	}
+	return(population)
+}
+
+# attention: only for 2D landscapes
+evaluate <- function(landscape,population) {
+	for(i in 1:nrow(population)) {
+		population[i,2] <- landscape(population[i,1])
+	}
+	return(population)
+}
+
+mutateOffspring <- function(population,mutateRate) {
+	newPopulation <- matrix(nrow=nrow(population)*2, ncol=ncol(population))
+	signs <- c(-1,1)
+	# copy old population
+	for(i in 1:nrow(population)) {
+		for(j in 1:(ncol(population)-1)) {
+			newPopulation[i,j] <- population[i,j]
+		}
+	}
+	# get new population
+	for(i in 1:nrow(population)) {
+		for(j in 1:(ncol(population)-1)) {
+			newPopulation[nrow(population)+i,j] <- population[i,j] + mutateRate * sample(signs,1,replace=TRUE)
+		}
+	}
+	return(newPopulation)
+}
+
+selectNextGen <- function(population,newSize) {
+	population <- population[order(population[,ncol(population)]),]
+	return(population[nrow(population):(nrow(population)-newSize),])
+}
+
+mainEA <- function(landscape, generations, candidates, gene, mutateRate, xMin, xMax, yMin, yMax) {
+	# each but the last column of the population presents one gene of each individual
+	# the last column of the population show the fitness of each individual
+	population <- initPopulation(candidates,gene,xMin,xMax)
+	population <- evaluate(landscape,population)
+	
+	# plot
+	plot(population[,1],population[,2],xlim=c(xMin,xMax),ylim=c(yMin,yMax))
+	seq <- seq(xMin,xMax,by=0.1) 
+	lines(seq,landscape(seq))
+	
+	generation = 1
+	while(generations > generation) {		
+		population <- mutateOffspring(population,mutateRate)
+		population <- evaluate(landscape,population)
+		population <- selectNextGen(population,candidates)
+		generation <- generation + 1
+		
+		# plot
+		plot(population[,1],population[,2],xlim=c(xMin,xMax),ylim=c(yMin,yMax))
+		seq <- seq(xMin,xMax,by=0.1) 
+		lines(seq,landscape(seq))
+	}
+	return(population)
+}
+
+# define functions (landscapes)
+borders <- function(x,y) {
+	if(x > 10 || x < 0) {
+		return(-Inf)
+	}
+	return(y)
+}
+
+g <- function(x) borders(x,x)
+k <- function(x) borders(x,sin(x))
+h <- function(x) borders(x,x*sin(x))
+l <- function(x) borders(x,2+cos(x)+sin(2*x))
+
+# settings
+landscape <- l
+generations <- 100
+sizeOfPopulation <- 10
+gene <- 1
+mutateRate <- 0.1
+xMin <-  0 # for first initalisation
+xMax <- 10 # for first initalisation
+yMin <- -10 # for plot
+yMax <-  10 # for plot
+
+# and go for it
+population <- mainEA(landscape, generations, sizeOfPopulation, gene, mutateRate, xMin, xMax, yMin, yMax)
---
+++ Evolutionaere Algorithmen/uebung3/aufgabe2/perceptron.R
@@ -0,0 +1,151 @@
+# settings
+generations <- 100
+sizeOfPopulation <- 10
+gene <- 3
+mutateRate <- 0.25
+mutateOnlyOneGene <- TRUE
+xMin <- -10 # for first initalisation and plot
+xMax <-  15 # for first initalisation and plot
+yMin <-  -5 # for plot
+yMax <-  15 # for plot
+
+# read data
+data <- read.table("data1.txt")
+x <- as.matrix(data[,1:(ncol(data)-1)])
+y <- as.vector(as.matrix(data[,ncol(data)]))
+
+# color points for ploting
+p <- as.matrix(data[,1:(ncol(data)-2)])
+p1_i <- 0
+p1 <- matrix(ncol=2, nrow=nrow(p))
+p2_i <- 0
+p2 <- matrix(ncol=2, nrow=nrow(p))
+for(i in 1:nrow(p)) {
+	if(y[i] > 0) {
+		p1_i <- p1_i + 1
+		p1[p1_i,] <- p[i,]
+	} else {
+		p2_i <- p2_i + 1
+		p2[p2_i,] <- p[i,]
+	}
+}
+
+# define landscapes (fitness function)
+landscape <- function(w) {
+#	# sum of false classified points
+#	delta <- 0
+#	for(i in 1:nrow(x)) {
+#		if(y[i] != sign(x[i,] %*% w)) {
+#			delta <- delta + 1
+#		}
+#	}
+#	
+#	# euclidean distance of false classified point
+#	delta <- 0
+#	for(i in 1:nrow(x)) {
+#		if(y[i] != sign(x[i,] %*% w)) {
+#			delta <- delta + sum((x[i,]-w)^2)
+#		}
+#	}
+
+	# euclidean distance of all classified point (weighted)
+	delta <- 0
+	for(i in 1:nrow(x)) {
+		if(y[i] != sign(x[i,] %*% w)) {
+			delta <- delta + 100*sum((x[i,]-w)^2)
+		} else {
+			delta <- delta - sum((x[i,]-w)^2)
+		}
+	}
+	return(delta)
+}
+
+# functions
+initPopulation <- function(candidates,gene,min,max) {
+	population <- matrix(nrow=candidates, ncol=(gene+1))
+	for(i in 1:candidates) {
+		for(j in 1:gene) {
+			population[i,j] <- runif(1,min,max)
+		}
+		population[i,gene+1] <- 0
+	}
+	return(population)
+}
+
+evaluate <- function(landscape,population) {
+	for(i in 1:nrow(population)) {
+		population[i,4] <- landscape(population[i,1:3])
+	}
+	return(population)
+}
+
+mutateOffspring <- function(population,mutateRate,gene) {
+	newPopulation <- matrix(nrow=nrow(population)*2, ncol=ncol(population))
+	signs <- c(-1,1)
+	# copy old population
+	for(i in 1:nrow(population)) {
+		for(j in 1:(ncol(population)-1)) {
+			newPopulation[i,j] <- population[i,j]
+		}
+	}
+	# get new population
+	for(i in 1:nrow(population)) {
+		if(mutateOnlyOneGene) {
+			for(j in 1:(ncol(population)-1)) {
+				newPopulation[nrow(population)+i,j] <- population[i,j]
+			}
+			j <- sample(1:gene,1)
+			newPopulation[nrow(population)+i,j] <- population[i,j] + mutateRate * sample(signs,1,replace=TRUE)
+		} else {
+			for(j in 1:(ncol(population)-1)) {
+				newPopulation[nrow(population)+i,j] <- population[i,j] + mutateRate * sample(signs,1,replace=TRUE)
+			}
+		}
+	}
+	return(newPopulation)
+}
+
+fpsSelection <- function(population,newSize) {
+	population <- population[order(population[,ncol(population)]),]
+	return(population[1:newSize,])
+}
+
+tournamentSelection <- function(population,newSize) {
+	k <- 4
+	newPopulation <- matrix(nrow=newSize, ncol=ncol(population))
+	for(i in 1:newSize) {
+		kIndividuals <- population[sample(1:ncol(population),k,replace=TRUE),]
+		kSorted <- kIndividuals[order(kIndividuals[,ncol(kIndividuals)]),]
+		newPopulation[i,] <- kSorted[1,]
+	}
+	return(newPopulation)
+}
+
+mainEA <- function(landscape, generations, candidates, gene, mutateRate, xMin, xMax, yMin, yMax) {
+	# each but the last column of the population presents one gene of each individual
+	# the last column of the population show the fitness of each individual
+	population <- initPopulation(candidates,gene,xMin,xMax)
+	population <- evaluate(landscape,population)	
+	generation = 1
+	while(generations > generation) {		
+		population <- mutateOffspring(population,mutateRate,gene)
+		population <- evaluate(landscape,population)
+		population <- fpsSelection(population,candidates)
+		#population <- tournamentSelection(population,candidates)
+		generation <- generation + 1
+		
+		# plot everything
+		plot(p1, xlim=c(xMin,xMax), ylim=c(yMin,yMax), pch=20, col="green")
+		points(p2, pch=20, col="blue")
+		x = seq(xMin,xMax,0.1)
+		for(i in 1:nrow(population)) {
+			w <- population[i,]
+			y = (w[3]-x*w[1])/w[2]   
+			lines(x,y)
+		}
+	}
+	return(population)
+}
+
+# and go for it
+population <- mainEA(landscape, generations, sizeOfPopulation, gene, mutateRate, xMin, xMax, yMin, yMax)
