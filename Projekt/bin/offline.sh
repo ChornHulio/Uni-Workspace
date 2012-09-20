52b1571c09042e35f6b1e3e6a7380fa561405364
chornhulio <chornhulio@web.de>
2012-09-20 19:47:26 +0200

    adjust pathes in offline.sh

--- Projekt/bin/offline.sh
+++ Projekt/bin/offline.sh
@@ -1,7 +1,8 @@
 #!/bin/bash
 
 # feature extraction settings
-folder="offline/mfccSvm007"
+folder="./test"
+folderWithSpeakers="../speaker/" # set this!!!
 speakerMin=1
 speakerMax=10
 extractionMethod="mfcc" # lpc or mfcc
@@ -12,7 +13,7 @@ window="hamming" # hamming or hann
 energyLevel=90
 
 # training settings
-trainingMethod="svm" # neuralGas or svm
+trainingMethod="neuralGas" # 'neuralGas' or 'svm'
 codebookSize=100
 iterations=10
 svmT=2 # 0=linear, 2=rbf
@@ -20,11 +21,11 @@ svmC=1
 svmG=1
 
 # create folder
-if [ -d ../test/$folder ]
+if [ -d $folder ]
 then
-    rm -r ../test/$folder
+    rm -r $folder
 fi
-mkdir ../test/$folder
+mkdir $folder
 
 for j in {0..2}
 do
@@ -37,51 +38,51 @@ do
 	for i in $(seq $speakerMin 1 $speakerMax)
 	do
 		# train features
-		java -jar FeatureExtraction.jar -i ../speaker/$i/$train1.wav -i ../speaker/$i/$train2.wav -l $i -o ../test/$folder/features.train -a --$extractionMethod -sw $sampleWidth -sr $slidingRate -c $coefficents -w $window -e $energyLevel
+		java -jar FeatureExtraction.jar -i $folderWithSpeakers$i/$train1.wav -i $folderWithSpeakers$i/$train2.wav -l $i -o $folder/features.train -a --$extractionMethod -sw $sampleWidth -sr $slidingRate -c $coefficents -w $window -e $energyLevel
 	
 		# test features
-		java -jar FeatureExtraction.jar -i ../speaker/$i/$test.wav -l $i -o ../test/$folder/features.test -a --$extractionMethod -sw $sampleWidth -sr $slidingRate -c $coefficents -w $window -e $energyLevel
+		java -jar FeatureExtraction.jar -i $folderWithSpeakers$i/$test.wav -l $i -o $folder/features.test -a --$extractionMethod -sw $sampleWidth -sr $slidingRate -c $coefficents -w $window -e $energyLevel
 	
 		# test features for analysis
-		java -jar FeatureExtraction.jar -i ../speaker/$i/$test.wav -l $i -o ../test/$folder/features.analysis -a --$extractionMethod -sw $sampleWidth -sr $slidingRate -c $coefficents -w $window -e $energyLevel
+		java -jar FeatureExtraction.jar -i $folderWithSpeakers$i/$test.wav -l $i -o $folder/features.analysis -a --$extractionMethod -sw $sampleWidth -sr $slidingRate -c $coefficents -w $window -e $energyLevel
 	done
 
 if [ "$trainingMethod" = "svm" ]
 then
 	# svm scale
 	echo "svm scale ($train1)"
-	./svm-scale -s ../test/$folder/scale ../test/$folder/features.train > ../test/$folder/features.train.scale
-	./svm-scale -r ../test/$folder/scale ../test/$folder/features.test > ../test/$folder/features.test.scale
+	./svm-scale -s $folder/scale $folder/features.train > $folder/features.train.scale
+	./svm-scale -r $folder/scale $folder/features.test > $folder/features.test.scale
 	
 	# svm training
 	echo "svm train ($train1)"
-	 ./svm-train -t $svmT -c $svmC -g $svmG -q ../test/$folder/features.train.scale ../test/$folder/model
+	 ./svm-train -t $svmT -c $svmC -g $svmG -q $folder/features.train.scale $folder/model
 	 
 	# svm prediction
 	echo "svm prediction ($train1)"
-	./svm-predict ../test/$folder/features.test.scale ../test/$folder/model ../test/$folder/tmpResult
+	./svm-predict $folder/features.test.scale $folder/model $folder/tmpResult
 	
 	# add tmpResult to result
-	cat ../test/$folder/tmpResult >> ../test/$folder/result
+	cat $folder/tmpResult >> $folder/result
 	
 	# remove unnecessary files
-	rm ../test/$folder/features.train
-	rm ../test/$folder/features.test
+	rm $folder/features.train
+	rm $folder/features.test
 else
 	# create codebook
 	echo "create codebook ($train1)"
-	java -jar CreateCodebook.jar -i ../test/$folder/features.train -o ../test/$folder/codebook -s $codebookSize -it $iterations
+	java -jar CreateCodebook.jar -i $folder/features.train -o $folder/codebook -s $codebookSize -it $iterations
 
 	# prediction
 	echo "prediciton ($train1)"
-	java -jar PredictByCodebook.jar -ff ../test/$folder/features.test -cf ../test/$folder/codebook -o ../test/$folder/result -a
+	java -jar NearestNeighbor.jar -ff $folder/features.test -cf $folder/codebook -o $folder/result -a
 
 	# remove unnecessary files
-	rm ../test/$folder/features.train
-	rm ../test/$folder/features.test
+	rm $folder/features.train
+	rm $folder/features.test
 fi
 done # end of outer for loop
 
 # analysis
 echo "analysis"
-java -jar AnalyseResult.jar -i ../test/$folder/features.analysis -p ../test/$folder/result -o ../test/$folder/analysis
+java -jar Analysis.jar -i $folder/features.analysis -p $folder/result -o $folder/analysis
